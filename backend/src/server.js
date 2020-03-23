5bc47d869163c215f254f985ddd683865fb8552d
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-03-23 16:56:16 -0300

    Implementação de notificação em tempo real no frontend
    Vídeo aula 05, 0:38:31
    
    Implementações no server:
    • Ajustes no server para enviar notificações realtime
    • Criadas novas rotas para "Aceitar" e "Rejeitar" uma solicitçaão de reserva
    
    Implementações no frontend:
    • Ajustar no frontend para receber notificações realtime quanto a solicitações de reserva
    • Interface para decidir por "Aceitar" ou "Rejeitar" uma solicitação de reserva

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,25 @@ const socketio = require('socket.io');
 const http     = require('http') ;
 
 const app    = express();
-const server = http.server(app);
+const server = http.Server(app);
 const io     = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@icfn-mpexq.mongodb.net/omnistack9?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
+})
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+    connectedUsers[user_id] = socket.id;    
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
 })
 
 // req.query  = Acessar Query Params (url?idade=20)       | indicado para filtros
--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,14 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import api from '../../services/api';
 
 import './styles.css';
 
 export default function Dashborad() {
     const [spots, setSpots ] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem('user');
+    
+    const socket  = useMemo(() => socketio('http://192.168.100.100:3333', {
+        query: {
+            user_id
+        }
+    }), [user_id])
+
+    useEffect(() => { 
+        socket.on('booking_request', data => {
+            setRequests([...requests, data])
+        })
+    }, [requests, socket]);
+
     useEffect(() => {
         async function loadSpots() {
-            const user_id = localStorage.getItem('user');
+            
             let response  = null;
             try {
                 console.log('Tentativa de obter spots do usuário id '+user_id)
@@ -28,8 +45,24 @@ export default function Dashborad() {
         loadSpots();
     }, []);
 
+    async function handleDecision(id, decision) {
+        await api.post(`/bookings/${id}/${decision ? 'approvals' : 'rejections'}`)
+        setRequests(requests.filter(request => request._id !== id))
+    }
+
     return (
         <>
+            {requests && (<ul className="notifications">
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong> 
+                        </p>
+                        <button className="accept" onClick={() => handleDecision(request._id,true)}>ACEITAR</button>
+                        <button className="reject" onClick={() => handleDecision(request._id,false)}>REJEITAR</button>
+                    </li>
+                ))}
+            </ul>)}
             <ul className="spot-list">
                 {spots && ( spots.map(spot => (
                     <li key={spot._id}>
